899226c0b3e5567b27e03c122a17434c5dbee8c9
WeiLong liu <93208385+Myid9606@users.noreply.github.com>
2024-05-03 20:03:43 +0800

    Delete mosi/tool.py

--- mosi/tool.py
+++
@@ -1,8 +0,0 @@
-# python设计/home/liuweilong/MMSA-FET/MOSI/label.csv转换为xls
-import pandas as pd
-
-# 读取CSV文件
-data = pd.read_csv('/home/liuweilong/MMSA-FET/MOSI/label.csv')
-
-# 将数据保存为XLS文件
-data.to_excel('/home/liuweilong/MMSA-FET/MOSI/label.xlsx', index=False)
